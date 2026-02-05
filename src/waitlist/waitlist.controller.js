20a046b9484b24d303d2d80d3f11d22dc4039aa7
olubiyi babajide <olubiyibabajide@gmail.com>
2026-02-05 22:34:10 +0100

    logging implemnented in service layer

--- src/waitlist/waitlist.controller.js
+++ src/waitlist/waitlist.controller.js
@@ -1,5 +1,6 @@
 import CreateWaitlist from "./waitlist.service.js";
 import ResponseHandler from "../utils/responseHandler.js";
+import Labels from "../utils/labels.js";
 
 class WaitListController{
 
@@ -8,6 +9,7 @@ class WaitListController{
         try{
             const {email,role} = req.body
             const user = await CreateWaitlist.waitlistEmail(email,role)
+            Labels.controllerLog.info(`${req.body} successfully added to waitlist`)
             return ResponseHandler.success(res,"Successfully added to the waitlist",user)
         }
         catch(error){
--- src/waitlist/waitlist.service.js
+++ src/waitlist/waitlist.service.js
@@ -1,6 +1,7 @@
 import AppError from "../errorHandlers/appError.js";
 import Waitlist from "./waitlist.model.js";
 import EmailsVerifications from "../helpers/sendEmails.js";
+import Labels from "../utils/labels.js";
 
 class WaitlistService{
 
@@ -10,27 +11,43 @@ class WaitlistService{
         const existingUser = await Waitlist.findOne({email})
 
         if(existingUser){
+                 Labels.serviceLog.warn(`${email} already exists`,{
+                email:email,
+                role:role
+            })
             throw new AppError("Email already exists",409)
+       
         }
 
         const user = await Waitlist.create({email,role})
+        Labels.serviceLog.info(`${email} successfully added to waitlist`,{
+            email:email,
+            role:role
+        })
 
         // send verification email
        try{
+        const emailLog = Labels.createLabel("emailVerifcationEmail")
         await EmailsVerifications.sendWaitlistWelcome(email,role)
+        emailLog.info(`email verifcation sent to: ${email}`,{
+            email:email
+        })
         console.log("Welcome email sent successfully");
         
        }
        catch(err){
         console.error("failed to send welcome mail",err);
-        
+        emailLog.error(`error sending verifcation mail to ${email}`)
        }
-       
         return user
     }
 
     static getWaitlistEmails = async()=>{
+        const getAllEmails = Labels.createLabel("allWaitlistEmail")
         const emails = await Waitlist.find()
+        getAllEmails.info("all emails retrieved",{
+            emails:emails
+        })
         return emails;
     }
 }
