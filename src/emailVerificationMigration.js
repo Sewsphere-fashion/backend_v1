e6b77e7d68d49f795e5b9e1a68aadce54fedfcb0
olubiyi babajide <olubiyibabajide@gmail.com>
2026-04-23 23:43:13 +0100

    fix:changed emailverification to use date instead of boolean

---
+++ src/emailVerificationMigration.js
@@ -0,0 +1,15 @@
+import connectDb from "./config/db.js";
+import User from "./Users/user.model.js";
+
+const emailVerifiedAtMigration = async () => {
+  try {
+    await connectDb();
+    const result = await User.updateMany({}, { $unset: { isVerified: "" } });
+    console.log(`unset done`);
+    process.exit(0);
+  } catch (err) {
+    console.log("error migrating");
+    process.exit(1);
+  }
+};
+emailVerifiedAtMigration();
